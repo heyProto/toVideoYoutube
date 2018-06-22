11939bc778f0c672979af4f8be27f63a21316e5c
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2018-06-22 20:32:11 +0530

    Updates SSR functions/

--- main_ssr.js
+++ main_ssr.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import Cluster from './src/js/card.jsx'
+import Card from './src/js/card.jsx'
 
 global.window = {}
 
+function renderWithMode(mode) {
+    switch (mode) {
+        case "col2":
+            return "x.renderCol2()";
+        case "col3":
+            return "x.renderCol3()";
+        case "col4":
+            return "x.renderCol4()";
+        case "col7":
+            return "x.renderCol7()";
+        case "col16":
+            return "x.renderCol16()";
+        default:
+            return "x.renderCol4()";
+    }
+}
+
+function getScriptString(mode, dataJSON, selector, site_configs) {
+    return `<script>
+            var x = new ProtoGraph.Card.toVideoYoutube(),
+                params = {
+                    "selector": document.querySelector('${selector}'),
+                    "isFromSSR": true,
+                    "initialState": ${JSON.stringify(dataJSON)},
+                    "site_configs": ${JSON.stringify(site_configs)}
+                };
+            x.init(params);
+            ${renderWithMode(mode)}
+        </script>
+    `
+}
+
 function render(mode, initialState) {
     let content = renderToString(
-        <Cluster
+        <Card
             dataJSON={initialState.dataJSON}
             mode={mode}
             renderingSSR={true}
@@ -17,5 +49,6 @@ function render(mode, initialState) {
 }
 
 module.exports = {
-    render: render
+    render: render,
+    getScriptString: getScriptString
 }
