f52d80a47f4b6370ebe99bbd898e003e75df8ec9
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2018-06-27 16:34:48 +0530

    SSR changes.

--- main_ssr.js
+++ main_ssr.js
@@ -2,43 +2,39 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import Card from './src/js/card.jsx'
 
-global.window = {}
+global.window = {};
 
-function getInstance(){
-    return new ProtoGraph.Card.toStory();
-}
+// function renderWithMode(mode) {
+//     switch (mode) {
+//         case "col2":
+//             return "x.renderCol2()";
+//         case "col3":
+//             return "x.renderCol3()";
+//         case "col4":
+//             return "x.renderCol4()";
+//         case "col7":
+//             return "x.renderCol7()";
+//         case "col16":
+//             return "x.renderCol16()";
+//         default:
+//             return "x.renderCol4()";
+//     }
+// }
 
-function renderWithMode(mode) {
-    switch (mode) {
-        case "col2":
-            return "x.renderCol2()";
-        case "col3":
-            return "x.renderCol3()";
-        case "col4":
-            return "x.renderCol4()";
-        case "col7":
-            return "x.renderCol7()";
-        case "col16":
-            return "x.renderCol16()";
-        default:
-            return "x.renderCol4()";
-    }
-}
-
-function getScriptString(mode, dataJSON, selector, site_configs) {
-    return `<script>
-            var x = new ProtoGraph.Card.toVideoYoutube(),
-                params = {
-                    "selector": document.querySelector('${selector}'),
-                    "isFromSSR": true,
-                    "initialState": ${JSON.stringify(dataJSON)},
-                    "site_configs": ${JSON.stringify(site_configs)}
-                };
-            x.init(params);
-            ${renderWithMode(mode)}
-        </script>
-    `
-}
+// function getScriptString(mode, dataJSON, selector, site_configs) {
+//     return `<script>
+//             var x = new ProtoGraph.Card.toVideoYoutube(),
+//                 params = {
+//                     "selector": document.querySelector('${selector}'),
+//                     "isFromSSR": true,
+//                     "initialState": ${JSON.stringify(dataJSON)},
+//                     "site_configs": ${JSON.stringify(site_configs)}
+//                 };
+//             x.init(params);
+//             ${renderWithMode(mode)}
+//         </script>
+//     `
+// }
 
 function render(mode, initialState) {
     let content = renderToString(
@@ -54,6 +50,6 @@ function render(mode, initialState) {
 
 module.exports = {
     render: render,
-    getScriptString: getScriptString,
-    getInstance: getInstance
+    // getScriptString: getScriptString,
+    instance: 'toVideoYoutube'
 }
